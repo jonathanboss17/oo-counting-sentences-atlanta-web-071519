706da40c3417afdb5ddc43f6b6d9cb255bb34126
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-18 18:33:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,6 +18,7 @@ class String
   def count_sentences
     x = []
     x = self.split(".", "!")
+    binding.pry
     puts x
   end
 end
@@ -26,7 +27,7 @@ end
 x = "My name is Jon. His name is Will Her name is Lucy?"
 y = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
 x.count_sentences
-binding.pry
+
 
 
 
