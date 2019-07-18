42af135ef9ef2e82f0fa3a1fe6db8e2f129f665d
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-18 18:26:50 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # ternary operators ... if ____ (is true) ? : (then return ) ____ : (else) ____
+    self.end_with?(".") ? true : false 
   end
 
   def question?
-
+    self.end_with?("?") ? true : false 
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(".", "!", "?")
   end
 end
+
+
+x = "My name is Jon. His name is Will Her name is Lucy?"
+y = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+# if(x.sentence?) 
+#   x.split(".").join
+# elsif(x.question?)
+# y =  x.split("?")
+# elsif(x.exclamation?)
+# y =  x.split("!")
+# end
+
+puts y.split(".").join.split("!").join.split("?")
+
+
+
+
+
+
+
+
+
